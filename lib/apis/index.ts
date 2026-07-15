bc26f1c1b4752ba178edb0dcb824cb9915097bbc
somehow <snsdkf6@gmail.com>
2026-07-16 03:44:41 +0900

    [feat] api lib 추가

---
+++ lib/apis/index.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+/** axios 공통함수 */
+const AxiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default AxiosInstance;
---
+++ lib/apis/postApis.ts
@@ -0,0 +1,9 @@
+import { LocationData } from "@/features/location/types/location";
+import AxiosInstance from "./index";
+
+/** location 데이터를 서버에 POST */
+export function postLocationToServer(locationData: LocationData) {
+  return AxiosInstance.post("/location", locationData).then(
+    (response) => response.data,
+  );
+}
